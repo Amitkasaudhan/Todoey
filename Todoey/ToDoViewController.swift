1c27ea65da1d777a35e7db6323492aa15cc501fe
Amit <amitkasaudhan115@gmail.com>
2019-07-26 00:58:23 +0530

    Check Uncheck

--- Todoey/ToDoViewController.swift
+++ Todoey/ToDoViewController.swift
@@ -17,9 +17,11 @@ class ToDoViewController: UITableViewController {
     }
 
 // ================================================
-//              TableViewDelegate
+//              TableViewDataSource
 //=================================================
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        
         return Array.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -27,5 +29,28 @@ class ToDoViewController: UITableViewController {
         cell.textLabel?.text = Array[indexPath.row]
         return cell
     }
-}
+// ================================================
+//              TableViewDataSource
+//=================================================
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        }
+        else{
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    
+    
+    
+    
+                 // Mark Add New item
+  //  @IBAction func AddPressButton(_ sender: Any) {
+        // alert =  UIAlertController(title: "Add New item", message: "String", preferredStyle: .de)
+        //let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what wil happe once usr click on add item button in UIAlert
+      //  }
+        //alert.ad
+    }
 
+}
